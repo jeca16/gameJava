696f5606e3c9ec4c1dfc61b8ddb57e9303dac456
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2023-08-30 16:32:35 -0300

    adicionar arquivos

--- src/br/senai/sp/jandira/game/game.java
+++ src/br/senai/sp/jandira/game/game.java
@@ -2,11 +2,14 @@ package br.senai.sp.jandira.game;
 
 import br.senai.sp.jandira.game.model.Register;
 
+import br.senai.sp.jandira.game.model.menu;
+
+
 public class game {
     public static void main(String[] args) {
-        Register register = new Register();
 
-        register.Decision();
+        menu menu = new menu();
 
+        menu.Menu();
     }
 }
